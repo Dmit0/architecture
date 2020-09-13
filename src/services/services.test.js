157d77e3409e0bad56601116ebbac34e903fc5e3
Dmitrii Shehunov <53045487+Dmit0@users.noreply.github.com>
2020-09-13 15:44:54 +0300

    Add files via upload

--- src/services/services.test.js
+++ src/services/services.test.js
@@ -1,20 +1,37 @@
-
+const {MongoClient} = require('mongodb');
 const userOperations=require('./userOperationServise')
 
 //const {MockUserSeviceCreate}=require('./mock')
 
 jest.mock('findOne')
 
-describe('tests of microservices',()=>{
-    describe('test with user microservices',()=>{
+describe('tests of services',()=>{
+    describe('test with user services',()=>{
         
-        describe('user create microservice',()=>{
-           beforeEach(()=>{
-                MockUserSeviceCreate.mockClear()
-           })
+        // let connection;
+        // let db;
+
+        // beforeAll(async () => {
+        //     connection = await MongoClient.connect(global.__MONGO_URI__, {
+        //       useNewUrlParser: true,
+        //     });
+        //     db = await connection.db(global.__MONGO_DB_NAME__);
+        //   });
+
+        //   afterAll(async () => {
+        //     await connection.close();
+        //     await db.close();
+        //   });
+
+        describe('user createservice',()=>{
+           
             test('should create a user',async()=>{
                 //const response = await userOperations.userCreate('qwert','23')
                 //console.log(response)
+                // const users = db.collection('users');
+                // const mockUser = {name: 'some-user-id', age: '123'};
+                // const insertedUser = await users.findOne({_id: 'some-user-id'});
+            
             })
             
             test('shouldnt create a user if such username exist',()=>{
