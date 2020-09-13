ac5a2b136859c7b20ff6677cda1f76df6da24765
Dmitrii Shehunov <53045487+Dmit0@users.noreply.github.com>
2020-09-13 15:24:46 +0300

    Update microservices.test.js

--- src/microservices/microservices.test.js
+++ src/microservices/microservices.test.js
@@ -5,13 +5,13 @@ describe('tests of microservices',()=>{
     describe('test with user microservices',()=>{
         describe('user create microservice',()=>{
             it('should create a user',async(done)=>{
-                const response = await userOperations.userCreate('qwert','23')
-                console.log(response)
+                //const response = await userOperations.userCreate('qwert','23')
+                //console.log(response)
             })
             it('shouldnt create a user if such username exist',async(done)=>{
-                await userOperations.userCreate('qwert','23')
-                const response1=await userOperations.userCreate('qwert','24')
-                console.log(response1)
+                //await userOperations.userCreate('qwert','23')
+                //const response1=await userOperations.userCreate('qwert','24')
+                //console.log(response1)
             })
         })
     })
