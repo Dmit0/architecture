2cd63fd70023090e821be7a85be7cf3b806c62c1
Dmitrii Shehunov <53045487+Dmit0@users.noreply.github.com>
2020-09-16 20:57:32 +0300

    Add files via upload

--- src/routes/user_router.js
+++ src/routes/user_router.js
@@ -1,27 +1,27 @@
 const {Router}=require('express')
 const user_router=Router()
 
-const { body,query } = require('express-validator');
+const { body,query,check } = require('express-validator');
 
-const UserControllers=require('../controllers/user_controllers')
+const UserControllers=require('../controllers/user_contollers')
 
 user_router.post('/create',[
-    body('name').notEmpty(),
-    body('age').notEmpty()
+    body('name','should be non-emptry').notEmpty(),
+    body('age','should be non-emptry').notEmpty()
 ],UserControllers.user_controller_create)
 
-user_router.get('/find:name',[
+user_router.get('/find',[
     query('name').notEmpty()
 ],UserControllers.user_controller_find)
 
 user_router.put('/update',[
-    body('name').notEmpty(),
-    body('updateName').notEmpty(),
-    body('updateAge').notEmpty(),
+    check('name','should be non-emptry').notEmpty(),
+    check('updateName','should be non-emptry').notEmpty(),
+    check('updateAge','should be non-emptry').notEmpty(),
 ],UserControllers.user_controller_update)
 
 user_router.delete('/delete',[
-    body('name').notEmpty()
+    check('name','should be non-emptry').notEmpty()
 ],UserControllers.user_controller_delete)
 
 
